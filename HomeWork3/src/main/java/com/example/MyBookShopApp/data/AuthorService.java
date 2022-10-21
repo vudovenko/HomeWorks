75f65b8238090e97f06284b01a5e0d045477a5c2
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-10-21 22:38:30 +0500

    HomeWork3 3

--- HomeWork3/src/main/java/com/example/MyBookShopApp/data/AuthorService.java
+++ HomeWork3/src/main/java/com/example/MyBookShopApp/data/AuthorService.java
@@ -5,15 +5,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.Map;
 
 @Service
 public class AuthorService {
 
     private JdbcTemplate jdbcTemplate;
-    Logger logger = Logger.getLogger(AuthorService.class.getName());
 
     @Autowired
     public AuthorService(JdbcTemplate jdbcTemplate) {
@@ -28,35 +27,18 @@ public class AuthorService {
             aut.setLastName(rs.getString("last_name"));
             return aut;
         }).get(0);
-//        logger.info("AuthorService.getAuthor: author = " + author);
         return author;
     }
 
-    public List<Author> getAuthorsData() {
-//        Map<Character, List<String>> authorsByFirstLettersName = new HashMap<>();
+    public Map<Character, List<String>> getAuthorsData() {
+        Map<Character, List<String>> authorsByFirstLettersName = new HashMap<>();
 
-//        for (int i = 128; i < 160; i++) {
-//            Character firstLetter = (char)i;
-//            List<String> authors = jdbcTemplate.query(String.format("SELECT * FROM authors WHERE first_name LIKE '%s'", firstLetter),
-//                    (ResultSet rs, int rowNum) -> {
-//                Author author = new Author();
-//                author.setId(rs.getInt("id"));
-//                author.setFirstName(rs.getString("first_name"));
-//                author.setLastName(rs.getString("last_name"));
-//                return author.toString();
-//            });
-//
-//            authorsByFirstLettersName.put(firstLetter, new )
-//        }
-
-        List<Author> authors = jdbcTemplate.query("SELECT * FROM authors", (ResultSet rs, int rowNum) -> {
-            Author author = new Author();
-            author.setId(rs.getInt("id"));
-            author.setFirstName(rs.getString("first_name"));
-            author.setLastName(rs.getString("last_name"));
-            return author;
-        });
-//        logger.info("AuthorService.getAuthorsData: authors count = " + authors.size());
-        return new ArrayList<>(authors);
+        for (Character firstLetter = 'A'; firstLetter <= 'Z'; firstLetter++) {
+            List<String> authors = jdbcTemplate.query("SELECT * FROM authors WHERE first_name LIKE \'" + firstLetter + "%\'",
+                    (ResultSet rs, int rowNum) ->
+                            rs.getString("first_name") + " " + rs.getString("last_name"));
+            authorsByFirstLettersName.put(firstLetter, authors);
+        }
+        return new HashMap<>(authorsByFirstLettersName);
     }
 }
