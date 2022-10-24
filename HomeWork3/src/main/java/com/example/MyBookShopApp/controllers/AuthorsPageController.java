a5bb3d342561991dc205429ecd87acaf0b974e8e
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-10-25 01:58:32 +0500

    hw3 fix

--- HomeWork3/src/main/java/com/example/MyBookShopApp/controllers/AuthorsPageController.java
+++ HomeWork3/src/main/java/com/example/MyBookShopApp/controllers/AuthorsPageController.java
@@ -1,11 +1,16 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.Author;
 import com.example.MyBookShopApp.data.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/bookshop")
@@ -18,9 +23,14 @@ public class AuthorsPageController {
         this.authorService = authorService;
     }
 
+    @ModelAttribute("authorsMap")
+    public Map<Character, List<String>> authorsMap() {
+        return authorService.getAuthorsData();
+    }
+
     @GetMapping("/authors")
-    public String authorsPage(Model model) {
-        model.addAttribute("authorData", authorService.getAuthorsData());
-        return "authors/index";
+    public String authorsPage(/*Model model*/) {
+//        model.addAttribute("authorData", authorService.getAuthorsData());
+        return "/authors/index";
     }
 }
