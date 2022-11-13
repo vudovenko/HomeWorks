fa42148cc69fdef1ae8edded138b7a64a22edb0f
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-11-13 16:16:07 +0500

    hw5 2/3

---
+++ HomeWork5/MyBookShopApp/src/main/java/com/example/MyBookShopApp/struct/SkillboxJavaFrameworkSpringCourseDataStructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyBookShopApp.struct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillboxJavaFrameworkSpringCourseDataStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillboxJavaFrameworkSpringCourseDataStructureApplication.class, args);
+    }
+
+}
