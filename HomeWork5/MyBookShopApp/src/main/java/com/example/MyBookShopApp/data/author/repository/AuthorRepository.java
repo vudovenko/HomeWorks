b5c41d51c31157dee389c61ea334d1423dbe25a9
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-11-13 16:25:37 +0500

    rem

--- HomeWork5/MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/author/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.MyBookShopApp.data.author.repository;
-
-import com.example.MyBookShopApp.data.author.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-}
